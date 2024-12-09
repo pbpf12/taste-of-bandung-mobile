5e4d99ce8dfd6aa927475b52b80e1338869a7a7c
rafiedel <rafie.asadel@ui.ac.id>
2024-12-09 13:21:59 +0700

    style: Remove unused imports

--- lib/features/search/logic/_logic.dart
+++ lib/features/search/logic/_logic.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:tasteofbandung/features/search/data/datasources/_datasources.dart';
