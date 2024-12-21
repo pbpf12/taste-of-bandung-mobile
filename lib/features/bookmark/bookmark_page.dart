ef9b55f430f6224f19e140e5f084bf07d124dba1
FigoFavian <figo21fr@gmail.com>
2024-12-21 20:35:55 +0700

    adding colours

--- lib/features/bookmark/bookmark_page.dart
+++ lib/features/bookmark/bookmark_page.dart
@@ -13,10 +13,12 @@ class BookmarkPage extends StatefulWidget {
 
 class _BookmarkPageState extends State<BookmarkPage> {
   Future<BookmarkEntry> fetchBookmarks(CookieRequest request) async {
-    final response = await request.get('http://${EndPoints().myBaseUrl}/last_activities/');
+    final response =
+        await request.get('http://${EndPoints().myBaseUrl}/last_activities/');
 
     // Check for authentication errors
-    if (response.containsKey('error') && response['error'] == 'User not authenticated') {
+    if (response.containsKey('error') &&
+        response['error'] == 'User not authenticated') {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('User not authenticated. Please log in.')),
       );
@@ -50,51 +52,69 @@ class _BookmarkPageState extends State<BookmarkPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bookmarks'),
+        backgroundColor: const Color.fromARGB(255, 157, 115, 93),
       ),
-      body: FutureBuilder<BookmarkEntry>(
-        future: fetchBookmarks(request),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                'Error: ${snapshot.error}',
-                style: const TextStyle(color: Colors.red),
-              ),
-            );
-          } else if (!snapshot.hasData || snapshot.data!.bookmarks.isEmpty) {
-            return const Center(
-              child: Text(
-                'No bookmarks available.',
-                style: TextStyle(fontSize: 18.0),
-              ),
-            );
-          } else {
-            final bookmarks = snapshot.data!.bookmarks;
-            return ListView.builder(
-              itemCount: bookmarks.length,
-              itemBuilder: (context, index) {
-                final bookmark = bookmarks[index];
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: ListTile(
-                    title: Text(bookmark.dishName),
-                    subtitle: Text(bookmark.restaurantName),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete, color: Colors.red),
-                      onPressed: () async {
-                        await deleteBookmark(request, bookmark.id);
-                        setState(() {});
-                      },
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFFFB845),
+              Color(0xFFFFC966),
+              Color(0xFFFFD57A),
+              Color(0xFFFFE38E),
+              Color(0xFFFFF0A1),
+              Color(0xFFFFEB84),
+              Color(0xFFFFF7C2),
+            ],
+          ),
+        ),
+        child: FutureBuilder<BookmarkEntry>(
+          future: fetchBookmarks(request),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Error: ${snapshot.error}',
+                  style: const TextStyle(color: Colors.red),
+                ),
+              );
+            } else if (!snapshot.hasData || snapshot.data!.bookmarks.isEmpty) {
+              return const Center(
+                child: Text(
+                  'No bookmarks available.',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              );
+            } else {
+              final bookmarks = snapshot.data!.bookmarks;
+              return ListView.builder(
+                itemCount: bookmarks.length,
+                itemBuilder: (context, index) {
+                  final bookmark = bookmarks[index];
+                  return Card(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ListTile(
+                      title: Text(bookmark.dishName),
+                      subtitle: Text(bookmark.restaurantName),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () async {
+                          await deleteBookmark(request, bookmark.id);
+                          setState(() {});
+                        },
+                      ),
                     ),
-                  ),
-                );
-              },
-            );
-          }
-        },
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
